97b0f7ed8752c52f20bf4dbf74b5a89c4f59de6c
Navodi S.Y.A.C. <syacnavodi420@gmail.com>
2025-04-06 01:30:29 +0530

    updated the nawaloka home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,27 +1,29 @@
 // App.jsx
-import React from 'react';
+import React from "react";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-import ChatInterface from './components/chatbot/ChatInterface';
-import BloodBank from './components/BloodBank/Home/home'
+import ChatInterface from "./components/chatbot/ChatInterface";
+import BloodBank from "./components/BloodBank/Home/home";
+import NawalokaHome from "./components/Nawaloka/NawalokaHome/nawalokahome";
+import NawalokaNavbar from "./components/Nawaloka/Navbar/navbar";
+import NawalokaClinics from "./components/Nawaloka/Clinic/clinicCard";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/bloodBank' element={<BloodBank/>}/>
+          <Route path="/" element={<NawalokaHome />} />
+          <Route path="/bloodBank" element={<BloodBank />} />
           <Route path="/chat" element={<ChatInterface />} />
-          
+          <Route path="/nawaloka" element={<NawalokaHome />} />
+          <Route path="/nawaloka/navbar" element={<NawalokaNavbar />} />
+          <Route path="/nawaloka/clinics" element={<NawalokaClinics />} />
         </Routes>
       </BrowserRouter>
     </div>
-
-
   );
 }
 
 export default App;
-
