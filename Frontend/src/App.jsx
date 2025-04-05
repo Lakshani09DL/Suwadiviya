9597de0b2eba7afbeaea8ff0aacf3a771a50bec2
Semini-Sawbhagya <161110096+Semini-Sawbhagya@users.noreply.github.com>
2025-04-05 09:36:15 +0530

    updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
-
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import './App.css';
 import ChatInterface from './components/ChatInterface';
+import BloodBank from './components/BloodBank/Home/home'
 
 function App() {
   return (
-    <div className="App">
-      
-      <ChatInterface/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/bloodBank' element={<BloodBank/>}/>
+      <Route path='/chat' element={<ChatInterface/>}/>
+    </Routes>
+    </BrowserRouter>
+   
   );
 }
 
 export default App;
+/*<div className="App">
+      <BloodBank/>
+      <ChatInterface/>
+    </div>*/
