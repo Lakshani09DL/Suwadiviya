97c8e953085e10ac893e16fc1df321638b3d7021
TharaniDJ <tharanij2002@gmail.com>
2025-04-07 11:21:30 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,49 +1,5 @@
-/*import React from 'react';
-import { BrowserRouter as Router, Route, Routes, RouterProvider, createBrowserRouter, Outlet, Navigate, BrowserRouter } from 'react-router-dom';
-
-import Home from "./gampaha/pages/home";
-import GampahaHome from "./gampaha/pages/gampaha_home";
-import Cliniclist from "./gampaha/pages/clinic_list";
-import Scanlist from "./gampaha/pages/scan_list";
-import Testlist from "./gampaha/pages/test_list";
-
-import './App.css';
-
-function App() {
-  const router = createBrowserRouter(
-    [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/gampaha",
-        element: <GampahaHome />,
-      },
-      {
-        path: "/gampaha/clinics",
-        element: <Cliniclist />,
-      },
-      {
-        path: "/gampaha/scans",
-        element: <Scanlist />,
-      },
-      {
-        path: "/gampaha/tests",
-        element: <Testlist />,
-      }
-    ]
-  )
-
-  return (
-    <RouterProvider router={router} />
-  );
-}
-
-export default App;*/
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, RouterProvider, createBrowserRouter, Outlet, Navigate, BrowserRouter } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from "./gampaha/pages/home";
 import GampahaHome from "./gampaha/pages/gampaha_home";
@@ -52,7 +8,6 @@ import Scanlist from "./gampaha/pages/scan_list";
 import Testlist from "./gampaha/pages/test_list";
 
 // Colombo Components
-
 import ColomboHome from "./colombo/pages/colombo_home";
 import ColomboCliniclist from "./colombo/pages/clinic_list";
 import ColomboScanlist from "./colombo/pages/scan_list";
@@ -61,47 +16,24 @@ import ColomboTestlist from "./colombo/pages/test_list";
 import './App.css';
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/gampaha",
-      element: <GampahaHome />,
-    },
-    {
-      path: "/gampaha/clinics",
-      element: <Cliniclist />,
-    },
-    {
-      path: "/gampaha/scans",
-      element: <Scanlist />,
-    },
-    {
-      path: "/gampaha/tests",
-      element: <Testlist />,
-    },
-    // Colombo Routes
-    {
-      path: "/colombo",
-      element: <ColomboHome />,
-    },
-    {
-      path: "/colombo/clinics",
-      element: <Cliniclist />,
-    },
-    {
-      path: "/colombo/scans",
-      element: <Scanlist />,
-    },
-    {
-      path: "/colombo/tests",
-      element: <Testlist />,
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gampaha" element={<GampahaHome />} />
+          <Route path="/gampaha/clinics" element={<Cliniclist />} />
+          <Route path="/gampaha/scans" element={<Scanlist />} />
+          <Route path="/gampaha/tests" element={<Testlist />} />
+          {/* Colombo Routes */}
+          <Route path="/colombo" element={<ColomboHome />} />
+          <Route path="/colombo/clinics" element={<ColomboCliniclist />} />
+          <Route path="/colombo/scans" element={<ColomboScanlist />} />
+          <Route path="/colombo/tests" element={<ColomboTestlist />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
