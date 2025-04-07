3fbb591c19a760f1722246b730681e6ac5b550d8
Semini-Sawbhagya <161110096+Semini-Sawbhagya@users.noreply.github.com>
2025-04-07 07:19:48 +0530

    updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import './App.css';
 import ChatInterface from './components/ChatInterface';
 import BloodBank from './components/BloodBank/Home/home'
-
+import Search from './components/BloodBank/Search/Search'
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/bloodBank' element={<BloodBank/>}/>
       <Route path='/chat' element={<ChatInterface/>}/>
+      <Route path='/search' element={<Search/>}/>
     </Routes>
     </BrowserRouter>
    
