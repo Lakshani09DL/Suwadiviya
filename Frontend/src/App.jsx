2526f9c205e9812a2e6b4a0499fdbe8500a5741e
Raleesa Perera <raleesasvperera@gmail.com>
2025-04-09 15:55:17 +0530

    Resolved App.jsx merge conflict by combining routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import {Route,Routes } from 'react-router-dom'
 import './App.css';
 import Login from './components/Login/login';
 import Register from './components/Register/register';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
     <Routes>
       <Route path='/login' element={<Login/>}/>
       <Route path='/register' element={<Register/>}/>
 
     </Routes>
-    </BrowserRouter>
+    </Router>
    
   );
 }
