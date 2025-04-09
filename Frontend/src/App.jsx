a74d1536223649693d565fab5c56dcb619318045
niduniDK <nidunikasige@gmail.com>
2025-04-09 23:08:09 +0530

    Create FrontEnd for Homagama Base Hospital

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, RouterProvider, createBrowserRouter, Outlet, Navigate, BrowserRouter } from 'react-router-dom';
+
+import Home from "./homagama/pages/home";
+
 
 import './App.css';
-import ChatInterface from './components/ChatInterface';
+import HomagamaHome from './homagama/pages/homagama_home';
+import Cliniclist from './homagama/pages/clininc_list';
+import Scanlist from './homagama/pages/scan_list';
+import Testlist from './homagama/pages/test_list';
+import ChatInterface from './components/ChatInterface'
 
 function App() {
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/chat",
+        element: <ChatInterface />,
+      },
+      {
+        path: "/homagama",
+        element: <HomagamaHome/>,
+      },
+      {
+        path: "/homagama/clinics",
+        element: <Cliniclist/>,
+      },
+      {
+        path: "/homagama/scans",
+        element: <Scanlist/>,
+      },
+      {
+        path: "/homagama/tests",
+        element: <Testlist/>,
+      }
+    ]
+  )
+
   return (
-    <div className="App">
-      
-      <ChatInterface/>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ Frontend/src/homagama/pages/test_list.jsx
@@ -0,0 +1,9 @@
+function Testlist(){
+    return(
+        <div>
+            Hello this is test page
+        </div>
+    );
+}
+
+export default Testlist;
