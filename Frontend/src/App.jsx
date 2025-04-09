4c514be1cd6afbcba82a6eef8b20df67b70df1e9
Raleesa Perera <raleesasvperera@gmail.com>
2025-04-09 15:12:00 +0530

    Update App.jsx content

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import './App.css';
 import ChatInterface from './components/ChatInterface';
-import BloodBank from './components/BloodBank/Home/home'
+import BloodBank from './components/BloodBank/Home/home';
+import Login from './components/Login/login';
+import Register from './components/Register/register';
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
     <Routes>
       <Route path='/bloodBank' element={<BloodBank/>}/>
       <Route path='/chat' element={<ChatInterface/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+
     </Routes>
     </BrowserRouter>
    
