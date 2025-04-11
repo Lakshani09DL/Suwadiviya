9c0fea35285b2a33fd503ccb56001a560767db49
Lakshani09DL <subhagyal2002@gmail.com>
2025-04-11 23:21:36 +0530

    Updated response router

--- Backend/services/response_router.py
+++ Backend/services/response_router.py
@@ -4,7 +4,7 @@ from services import emergency, hospital_info,nearest_hospital
 async def route_to_handler(intent: str, query: str):
     if intent == "emergency_advice":
         return await emergency.handle_emergency(query)
-    elif intent == "hospital_info":
+    elif intent == "hospital_info" or "test_info":
         return await hospital_info.answer_hospital_info(query)
     elif intent == "clinic_schedule":
         return await hospital_info.answer_clinic_schedule(query)
