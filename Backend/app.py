b52301dd8cb502c884a5bf1cbc200bebad5df596
Lakshani09DL <subhagyal2002@gmail.com>
2025-04-13 11:21:35 +0530

    Updated requirements.txt

--- Backend/app.py
+++ Backend/app.py
@@ -16,7 +16,7 @@ from mongodb import init_db
 
 
 
-#from routers import chatbot, blood_bank
+from routers import chatbot, blood_bank
 from tasks.notifications import router as notifications_router  # Import the router
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
@@ -44,7 +44,7 @@ async def startup_db_client():
 
 
 
-#app.include_router(chatbot.router, prefix='/chatbot', tags=['Chatbot'])
+app.include_router(chatbot.router, prefix='/chatbot', tags=['Chatbot'])
 #app.include_router(blood_bank.router, prefix='/blood_bank', tags=['Blood Bank'])
 app.include_router(notifications_router, prefix="/notifications", tags=["Notifications"])
 
