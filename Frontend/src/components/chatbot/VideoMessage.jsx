1e2ece8cfdf3141a558a15fbc6a1772a563ccc89
niduniDK <nidunikasige@gmail.com>
2025-04-24 11:12:22 +0530

    Add styles to VideoMessage output

--- Frontend/src/components/chatbot/VideoMessage.jsx
+++ Frontend/src/components/chatbot/VideoMessage.jsx
@@ -2,24 +2,24 @@
 
 const VideoMessage = ({ videos }) => {
     return (
-      <div className="flex overflow-x-auto space-x-4 p-2 mb-4 scrollbar-thin scrollbar-thumb-gray-300">
-        {videos.map((video, index) => (
-          <a
-            key={index}
-            href={video.url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="min-w-[200px] flex-shrink-0 bg-white shadow rounded-xl overflow-hidden hover:shadow-lg transition"
-          >
-            <img
-              src={video.thumbnail}
-              alt={video.title}
-              className="w-full h-32 object-cover"
-            />
-            
-            <div className="p-2 text-sm font-medium">{video.title}</div>
-          </a>
-        ))}
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-2 mb-4">
+      {videos.map((video, index) => (
+        <a
+        key={index}
+        href={video.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="min-w-[200] min-h-[150] h-full bg-white shadow rounded-xl overflow-hidden hover:shadow-lg transition"
+        >
+        <img
+          src={video.thumbnail}
+          alt={video.title}
+          className="w-full h-32 object-cover"
+        />
+        
+        <div className="p-2 text-sm font-medium">{video.title}</div>
+        </a>
+      ))}
       </div>
     );
   };
